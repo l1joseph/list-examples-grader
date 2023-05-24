fafc9a193806a10743616fafe59430a44302fb69
Meshall Alshalaan <cs15lsp23pc@ieng6-201.ucsd.edu>
2023-05-24 16:34:47 -0700

    finished grade.sh

--- grade.sh
+++ grade.sh
@@ -1,38 +1,40 @@
-CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
 set -e
+
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
-files=`find student-submission/ -name "*java"` 
+git clone $1 student-submission 2> cloneTrash.txt
+echo "Finished Cloning"
+files=`find student-submission/ -name "*java"`
 for file in $files
 do
-    if [[ $file == "student-submission/ListExamples.java" ]]
-    then
-        echo 'File Found'
-        cp $file grading-area/
-    else
-        echo 'File Missing/Incorrect File Name'
-        exit
-    fi
+	if [[ $file == "student-submission/ListExamples.java" ]]
+	then
+		echo "File Found"
+		cp $file grading-area/
+	else
+		echo "File Missing/Incorrect File Name"
+		exit
+	fi
 done
 
 cp -r TestListExamples.java grading-area/
 cp -R lib/ grading-area/
-cd grading-area/
-javac -cp $CPATH ListExamples.java TestListExamples.java 
-if [[ $? -ne 0 ]]
-then  
-    echo 'error'
-    exit 1
-else
-    echo 'compile muy bueno'
-fi
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+cd grading-area
 
+set +e
+javac -cp $CPATH *.java 2> trash.txt
+if [[ "$?" -ne 0 ]]
+then 
+	echo "File failed to compile"
+	exit
+fi
+set -e
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
